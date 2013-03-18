f83d75f8a057bebf76a833a8c6fc1502708d9a88
stefanthoss <stef@nthoss.de>
2013-03-17 20:22:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterStockMood::Application.config.secret_token = '99cd69328e1375e7bb583b252b38f14474bcd0af03bf02306b4f444ee7b3de8a02f14727c219db0dcdd7bd1551df6c62fbb2d2b3ca3191f4e0da46fc4f4f149c'
